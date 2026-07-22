02ea81d89d7688bf94c356630adf994f787e39e9
verma <Vermagourav201@gmail.com>
2026-07-22 13:32:02 +0530

    Day 2: Project skeleton - Spring Boot backend + frontend shell + health endpoint

---
+++ src/main/java/com/vshield/vshield/VshieldApplication.java
@@ -0,0 +1,13 @@
+package com.vshield.vshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VshieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VshieldApplication.class, args);
+	}
+
+}
